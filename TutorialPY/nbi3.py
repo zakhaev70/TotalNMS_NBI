5bb9270b942d79a00d190d3ae0d8fcb01de98427
JaviSorribes <jsorribe@hawk.iit.edu>
2017-07-11 13:51:20 +0200

    Take mgid as input

---
+++ TutorialPY/nbi3.py
@@ -0,0 +1,24 @@
+import requests
+import xml.etree.ElementTree as ET
+
+host = 'http://172.19.254.3/ws'
+
+with requests.Session() as s:
+    s.auth=('admin','manager')
+    s.headers.update({'Content-Type': 'text/xml;charset=UTF-8', 
+            'SOAPAction': '', 'Connection': 'keep-alive'})
+
+    with open('./getCPEsByManagedGroup.xml','r') as f:
+        body = f.read()
+
+    mgid = input('Enter Managed Group ID: ')
+    assert(mgid.isdigit())
+    xmlbody = ET.fromstring(body)
+    xmlbody[1][0][0].text = mgid  #managedGroupId tag gets new value
+    body = ET.tostring(xmlbody)
+
+    r = s.post(host+'/cpeService', data=body)
+    resp = ET.fromstring(r.text)
+    numcpes = resp[0][0].find('return/totalNumber')
+    print(numcpes.tag, numcpes.text, sep=': ')
+  
