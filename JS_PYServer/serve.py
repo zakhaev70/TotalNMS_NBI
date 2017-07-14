340cb0da6f81b9dcf67df9e5c7022ffbd7319fd0
JaviSorribes <jsorribe@hawk.iit.edu>
2017-07-14 12:27:39 +0200

    CLN: remove unnecessary stuff

--- JS_PYServer/serve.py
+++ JS_PYServer/serve.py
@@ -8,7 +8,6 @@
 # Must send authorization credentials as header
 #
 # Tested with JavaScript and other Python3 applications
-#
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import requests
@@ -58,7 +57,7 @@ class NBIConnHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.send_header('Access-Control-Allow-Origin', self.headers['Origin'])  #or '*' for all origins
-        self.send_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')  #or self.headers['Access-Control-Request-Method'])
+        self.send_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
         self.send_header('Access-Control-Allow-Headers', self.headers['Access-Control-Request-Headers'])
         #self.send_header('Access-Control-Max-Age', '86400')  #OPTIONS req cached for 24h
         self.send_header('Access-Control-Allow-Credentials', 'true')
